0e90436c49e84e0242ca5b1429e17528b2782ddb
Flavio <flavioantonio5@hotmail.com>
2022-02-20 16:49:29 -0300

    sentry

--- pypro/settings.py
+++ pypro/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from functools import partial
 from pathlib import Path
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # import cloudinary.api  # cloudinary
 import dj_database_url
@@ -149,3 +151,18 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SENTRY_DSN = config('SENTRY_DSN', default=None)
+
+if SENTRY_DSN:
+    sentry_sdk.init(dsn=SENTRY_DSN, integrations=[DjangoIntegration()],
+
+                    # Set traces_sample_rate to 1.0 to capture 100%
+                    # of transactions for performance monitoring.
+                    # We recommend adjusting this value in production.
+                    traces_sample_rate=1.0,
+
+                    # If you wish to associate users to errors (assuming you are using
+                    # django.contrib.auth) you may enable sending PII data.
+                    send_default_pii=True
+                    )
