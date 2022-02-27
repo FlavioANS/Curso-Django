f25212f7b8f3bf4ec6c36a0dcb1136ca50210fd8
Flavio <flavioantonio5@hotmail.com>
2022-02-26 21:56:36 -0300

    metodo template

---
+++ pypro/aperitivos/tests/tests_indice.py
@@ -0,0 +1,36 @@
+import pytest
+from django.urls import reverse
+
+from pypro.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('aperitivos:indice'))
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+@pytest.mark.parametrize(
+    'slug',
+    [
+        'Video Aperitivo: Motivação',
+        'Instalação Windows'
+    ]
+)
+def test_titulo_video(resp, slug):
+    assert_contains(resp, slug)
+
+
+@pytest.mark.parametrize(
+    'slug',
+    [
+        'motivacao',
+        'instalacao-windows'
+    ]
+)
+def test_link_video(resp, slug):
+    video_link = reverse('aperitivos:video', args=(slug,))
+    assert_contains(resp, f'href="{video_link}"')
