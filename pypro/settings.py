70fcb7b3aa1c40ad7ce1cebbc9f0045a307e707f
Flavio <flavioantonio5@hotmail.com>
2022-03-03 19:11:00 -0300

    processador de contexto

--- pypro/settings.py
+++ pypro/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pypro.base',
     'pypro.aperitivos',
+    'pypro.modulos',
 ]
 
 MIDDLEWARE = [
@@ -74,6 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'pypro.modulos.context_processors.listar_modulos',
             ],
         },
     },
