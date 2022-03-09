320232fbde40a37db135b5eb99eca70424c35816
Flavio <flavioantonio5@hotmail.com>
2022-03-08 21:17:57 -0300

    Correçao

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -4,4 +4,9 @@ from pypro.modulos import facade
 
 def detalhe(request, slug):
     modulo = facade.econtrar_modulo(slug)
-    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo})
+    aulas = facade.listar_aulas_de_modulo_ordenadas(modulo)
+    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
+
+
+def aula(request, slug):
+    pass
