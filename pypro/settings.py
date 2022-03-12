e95bc72fbab77e45e2ff4ee86c7b93053b47c7e5
Flavio <flavioantonio5@hotmail.com>
2022-03-12 16:17:18 -0300

    Admin com modelos relacionados

--- pypro/settings.py
+++ pypro/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'pypro.base',
+    'pypro.turmas',
     'pypro.aperitivos',
     'pypro.modulos',
     'ordered_model',
