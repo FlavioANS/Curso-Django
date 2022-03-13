75dcd3f79d9aa0a31ef293d428dd97b90be9808b
Flavio <flavioantonio5@hotmail.com>
2022-03-13 14:37:18 -0300

    Inserçao botao login

---
+++ pypro/base/tests/test_login.py
@@ -0,0 +1,47 @@
+from django.urls import reverse
+import pytest
+from model_bakery import baker
+
+from pypro.django_assertions import assert_contains
+
+
+@pytest.fixture()
+def resp(client, db):
+    return client.get(reverse('login'))
+
+
+def test_login_from_page(resp):
+    assert resp.status_code == 200
+
+
+@pytest.fixture
+def usuario(db, django_user_model):
+    usuario_modelo = baker.make(django_user_model)
+    senha = 'senha'
+    usuario_modelo.set_password(senha)
+    usuario_modelo.save()
+    usuario_modelo.senha_plana = senha
+    return usuario_modelo
+
+
+@pytest.fixture
+def resp_post(client, usuario):
+    return client.post(reverse('login'), {'username': usuario.email, 'password': usuario.senha_plana})
+
+
+def test_login_redirect(resp_post):
+    assert resp_post.status_code == 302
+    assert resp_post.url == reverse('modulos:indice')
+
+
+@pytest.fixture()
+def resp_home(client, db):
+    return client.get(reverse('base:home'))
+
+
+def test_botao_entrarr_disponivel(resp_home):
+    assert_contains(resp_home, 'Entrar')
+
+
+def test_link_de_login_disponivel(resp_home):
+    assert_contains(resp_home, reverse('login'))
