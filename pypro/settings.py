65f44fe2bbe52ab976ebb22e1db2a53f2effef3e
Flavio <flavioantonio5@hotmail.com>
2022-03-16 19:31:09 -0300

    Recuperação de Senhas

--- pypro/settings.py
+++ pypro/settings.py
@@ -42,6 +42,10 @@ LOGOUT_REDIRECT_URL = '/'
 # Application definition
 
 INSTALLED_APPS = [
+    'pypro.base',
+    'pypro.turmas',
+    'pypro.aperitivos',
+    'pypro.modulos',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -50,10 +54,6 @@ INSTALLED_APPS = [
     'collectfast',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
-    'pypro.base',
-    'pypro.turmas',
-    'pypro.aperitivos',
-    'pypro.modulos',
     'ordered_model',
     'django_extensions',
 ]
