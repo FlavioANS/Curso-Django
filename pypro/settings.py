5ce5ffecc4172621c36cb484eea85616d3fc3834
Flavio <flavioantonio5@hotmail.com>
2022-03-16 20:58:02 -0300

    Configuraçao envio de emial no servidor

--- pypro/settings.py
+++ pypro/settings.py
@@ -90,6 +90,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pypro.wsgi.application'
 
+# Configuraçao de envio de Email
+
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+
+
 # Configurando Django debug Toolbar
 
 INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
